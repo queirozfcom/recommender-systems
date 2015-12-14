f4fe8efa97b1d1739e2ea7b680985d93d70798f4
felipe <falmeida1988@gmail.com>
2015-12-14 02:27:12 -0200

    final commit

--- code/python/src/lib/features.py
+++ code/python/src/lib/features.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sklearn.preprocessing import scale,OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder,MaxAbsScaler,StandardScaler,MinMaxScaler
 from src.lib.date import day_of_week_from_timestamp
 from src.lib.date import day_of_month_from_timestamp
 from src.lib.date import am_pm_from_timestamp
@@ -16,35 +16,57 @@ def transform_time_column(column,strategy):
         strategy: one of:
             False,       
             "naive",
-            "standardized",
-            "day_of_week",
-            "day_of_month",
-            "am_pm"       
+            "standard_scaler",
+            "day_of_week_ohe",
+            "day_of_month_ohe",
+            "am_pm_ohe"
+
+    Note:
+        "ohe" stands for one-hot encoding               
     """
     if strategy == False:
         return(_identity(column))
     else:
         if strategy == "naive":
             return(_naive(column))
-        elif strategy == "standardized":
-            return(_standardized(column))
-        elif strategy == "day_of_week":
+        elif strategy == "standard_scaler":
+            return(_standard_scaler(column))
+        elif strategy == "max_abs_scaler":
+            return(_max_abs_scaler(column))
+        elif strategy == "min_max_scaler":
+            return(_min_max_scaler(column))    
+        elif strategy == "day_of_week_ohe":
             return(_day_of_week(column))
-        elif strategy == "day_of_month":
+        elif strategy == "day_of_month_ohe":
             return(_day_of_month(column))
-        elif strategy == "am_pm":
+        elif strategy == "am_pm_ohe":
             return(_am_pm(column))   
         else:
             raise ValueError("Invalid strategy: {0}".format(strategy))          
 
 def _identity(column):
     return(column.reshape(-1,1))
-    
+
 def _naive(column):
     return(column.reshape(-1,1))
 
-def _standardized(column):
-    return(scale(column).reshape(-1,1))
+def _standard_scaler(column):
+    sc = StandardScaler()
+    sc.fit(column.reshape(-1,1))
+    new_col = sc.transform(column.reshape(-1,1))
+    return(new_col)
+
+def _max_abs_scaler(column):
+    sc = MaxAbsScaler()
+    sc.fit(column.reshape(-1,1))
+    new_col = sc.transform(column.reshape(-1,1))
+    return(new_col)
+
+def _min_max_scaler(column):
+    sc = MinMaxScaler()
+    sc.fit(column.reshape(-1,1))
+    new_col = sc.transform(column.reshape(-1,1))
+    return(new_col)
 
 def _day_of_week(column):
     v_func = np.vectorize(day_of_week_from_timestamp)
